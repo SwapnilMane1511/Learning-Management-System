eaf002b851162f591b0a79f360f53f3d8ce0d915
Swapnil Mane <swapnilmane721@gmail.com>
2025-06-30 19:57:26 +0530

    Update courseProgressApi.js

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+const BASE_URL = process.env.BASE_URL;
+const COURSE_PROGRESS_API = `${BASE_URL}/progress`;
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
