684aae3b5ca8d35475a9dbd12396a97f3951e20d
SwapnilMane1511 <swapnilmane721@gmail.com>
2025-07-02 20:01:07 +0530

    changes done

--- server/controllers/coursePurchase.controller.js
+++ server/controllers/coursePurchase.controller.js
@@ -39,8 +39,8 @@ export const createCheckoutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
-      cancel_url: `http://localhost:5173/course-detail/${courseId}`,
+      success_url: `https://learning-management-system-4-0rf2.onrender.com//${courseId}`, // once payment successful redirect to course progress page
+      cancel_url: `https://learning-management-system-4-0rf2.onrender.com//${courseId}`,
       metadata: {
         courseId: courseId,
         userId: userId,
